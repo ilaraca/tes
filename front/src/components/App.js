ce1925acb3f98a34bc3e6bed27f2413f67c6fe0c
Ilara Almeida <ilaraca@gmail.com>
2019-03-07 14:00:13 -0300

    done

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1> My React App!sdfsdfsdf</h1>
+        <h1> My React App!sdfsdfsdfasdasdas</h1>
       </div>
     );
   }
